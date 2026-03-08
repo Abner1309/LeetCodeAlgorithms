88da5d0bb8390bdc4fbbe31746129d7bf4b77cf4
Abner Barradas <159132346+Abner1309@users.noreply.github.com>
2026-03-08 15:33:33 -0300

    Partial Resolution

--- 23) MergeKSortedLists/Solution.java
+++ 23) MergeKSortedLists/Solution.java
@@ -1,84 +1,73 @@
 import java.util.*;
 
 public class Solution {
-  private int linkedListLength(ListNode head) {
-    int answer = 0;
-    ListNode aux = head;
+    private int linkedListLength(ListNode head) {
+        int answer = 0;
+        ListNode aux = head;
 
-    while (aux != null) {
-      aux = aux.next;
-      answer++;
-    }
+        while (aux != null) {
+            aux = aux.next;
+            answer++;
+        }
 
-    return answer;
-  }
-  
-  public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-    int len1 = linkedListLength(list1);
-    int len2 = linkedListLength(list2);
-    if (len1 == 0 && len2 == 0) return null;
-
-    ListNode answer = new ListNode();
-    ListNode answerPointer = answer;
-    for (int i = 1; i < len1 + len2; i++) {
-      answerPointer.next = new ListNode();
-      answerPointer = answerPointer.next;
+        return answer;
     }
 
-    ListNode pointer1 = list1;
-    ListNode pointer2 = list2;
-    answerPointer = answer;
-    while (pointer1 != null || pointer2 != null) {
-      if (pointer1 == null) {
-        answerPointer.val = pointer2.val;
-        answerPointer = answerPointer.next;
-        pointer2 = pointer2.next;
-      }
-      else if (pointer2 == null) {
-        answerPointer.val = pointer1.val;
-        answerPointer = answerPointer.next;
-        pointer1 = pointer1.next;
-      }
-      else if (pointer1.val < pointer2.val) {
-        answerPointer.val = pointer1.val;
-        answerPointer = answerPointer.next;
-        pointer1 = pointer1.next;
-      }
-      else {
-        answerPointer.val = pointer2.val;
-        answerPointer = answerPointer.next;
-        pointer2 = pointer2.next;
-      }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        int len1 = linkedListLength(list1);
+        int len2 = linkedListLength(list2);
+        if (len1 == 0 && len2 == 0) return null;
+
+        ListNode answer = new ListNode();
+        ListNode answerPointer = answer;
+        for (int i = 1; i < len1 + len2; i++) {
+            answerPointer.next = new ListNode();
+            answerPointer = answerPointer.next;
+        }
+
+        ListNode pointer1 = list1;
+        ListNode pointer2 = list2;
+        answerPointer = answer;
+        while (pointer1 != null || pointer2 != null) {
+            if (pointer1 == null) {
+                answerPointer.val = pointer2.val;
+                answerPointer = answerPointer.next;
+                pointer2 = pointer2.next;
+            }
+            else if (pointer2 == null) {
+                answerPointer.val = pointer1.val;
+                answerPointer = answerPointer.next;
+                pointer1 = pointer1.next;
+            }
+            else if (pointer1.val < pointer2.val) {
+                answerPointer.val = pointer1.val;
+                answerPointer = answerPointer.next;
+                pointer1 = pointer1.next;
+            }
+            else {
+                answerPointer.val = pointer2.val;
+                answerPointer = answerPointer.next;
+                pointer2 = pointer2.next;
+            }
+        }
+
+        return answer;
     }
 
-    return answer;
-  }
-  
-  public ListNode mergeKLists(ListNode[] lists) {
-    ListNode[] answer = lists;
-    int qtdLists = lists.length;
-  
-    if (qtdLists == 0) {
-      return null;
+    public ListNode mergeKLists(ListNode[] lists) {
+        ArrayList<ListNode> nodes = new ArrayList<ListNode>(Arrays.asList(lists));
+
+        int i = 1;
+        while (i < nodes.size()) {
+            ListNode first = nodes.getFirst();
+            ListNode second = nodes.get(i);
+            nodes.set(0, mergeTwoLists(first, second));
+        }
+
+        return nodes.getFirst();
     }
-    else if (qtdLists == 1) {
-      return lists[0];
+
+    public static void main (String[] args) {
+        // code
     }
-    else {
-      int i = 0, j = 1;
-      while (j < qtdLists) {
-        ListNode aux1 = answer[i];
-        ListNode aux2 = answer[j];
-        answer[i] = mergeTwoLists(aux1, aux2);
-        i = i + 2;
-        j = j + 2;
-      }
-    }
-    
-    return answer[0];
-  }
-  
-  public static void main (String[] args) {
-    // code
-  }
 }
