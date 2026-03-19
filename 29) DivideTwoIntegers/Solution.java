9bcc6de2356bcae0806238dc5e2791d3be932148
Abner1309 <soul_abner@hotmail.com>
2026-03-19 14:53:25 -0300

    Solution

--- 29) DivideTwoIntegers/Solution.java
+++ 29) DivideTwoIntegers/Solution.java
@@ -1,14 +1,10 @@
 public class Solution {
-    private int divideExtreme(int dividend, int divisor) {
-        // pass
-    }
-
-    private int dividePositive(int dividend, int divisor) {
+    private int dividePositivePositive(int dividend, int divisor) {
         int count = 0;
 
-        if (Math.abs(divisor) == 1) { return Math.abs(dividend); }
+        if (divisor == 1) { return dividend; }
 
-        int aux1 = Math.abs(dividend), aux2 = Math.abs(divisor);
+        int aux1 = dividend, aux2 = divisor;
         while (aux1 >= aux2) {
             aux1 = aux1 - aux2;
             count++;
@@ -17,15 +13,44 @@ public class Solution {
         return count;
     }
 
-    private int divideNegative(int dividend, int divisor) {
+    private int dividePositiveNegative(int dividend, int divisor) {
         int count = 0;
 
-        if (Math.abs(divisor) == 1) { return -Math.abs(dividend); }
+        if (divisor == -1) { return Math.negateExact(dividend); }
 
-        int aux1 = Math.abs(dividend), aux2 = Math.abs(divisor);
-        while (aux1 >= aux2) {
+        int aux1 = Math.negateExact(dividend), aux2 = divisor;
+        while (aux1 <= aux2) {
             aux1 = aux1 - aux2;
             count--;
+        }
+
+        return count;
+    }
+
+    private int divideNegativePositive(int dividend, int divisor) {
+        int count = 0;
+
+        if (divisor == 1) { return dividend; }
+
+        int aux1 = dividend, aux2 = Math.negateExact(divisor);
+        while (aux1 <= aux2) {
+            aux1 = aux1 - aux2;
+            count--;
+        }
+
+        return count;
+    }
+
+    private int divideNegativeNegative(int dividend, int divisor) {
+        int count = 0;
+
+        if (dividend == Integer.MIN_VALUE && divisor == -1) { return Integer.MAX_VALUE; }
+        if (divisor == -1) { return Math.negateExact(dividend); }
+
+        int aux1 = dividend, aux2 = divisor;
+        while (aux1 <= aux2) {
+            aux1 = aux1 - aux2;
+            count++;
         }
 
         return count;
@@ -34,10 +59,10 @@ public class Solution {
     public int divide(int dividend, int divisor) {
         int answer = 0;
 
-        if (dividend >= 0 && divisor >= 0) { answer = dividePositive(dividend, divisor); }
-        else if (dividend >= 0 && divisor < 0) { answer = divideNegative(dividend, divisor); }
-        else if (dividend < 0 && divisor >= 0) { answer = divideNegative(dividend, divisor); }
-        else { answer = dividePositive(dividend, divisor); }
+        if (dividend >= 0 && divisor >= 0) { answer = dividePositivePositive(dividend, divisor); }
+        else if (dividend >= 0 && divisor < 0) { answer = dividePositiveNegative(dividend, divisor); }
+        else if (dividend < 0 && divisor >= 0) { answer = divideNegativePositive(dividend, divisor); }
+        else { answer = divideNegativeNegative(dividend, divisor); }
 
         return answer;
     }
