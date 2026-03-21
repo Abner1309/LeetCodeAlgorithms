2eb7b5366b0929e86a2f6a5074a457f445ba88cf
Abner Barradas <159132346+Abner1309@users.noreply.github.com>
2026-03-21 11:08:05 -0300

    In process

--- 30) SubstringWithConcatenationOfAllWords/Solution.java
+++ 30) SubstringWithConcatenationOfAllWords/Solution.java
@@ -1,22 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
-    private static boolean checkSubstring(String subStr, String[] words) {
+    private static boolean checkSubString(int len, String target, String[] words) {
+        StringBuilder auxiliary = new StringBuilder();
+        ArrayList<String> listWords = new ArrayList<>(List.of(words));
 
+        for (int i = 0; i < target.length(); i = i + len) {
+            auxiliary.append(target, i, i + len);
+            if (listWords.contains(auxiliary.toString())) {
+                listWords.remove(auxiliary.toString());
+                auxiliary.setLength(0);
+            }
+            else { return false; }
+        }
+
+        return true;
     }
 
     public static List<Integer> findSubstring(String s, String[] words) {
-        StringBuilder answer = new StringBuilder();
+        List<Integer> answer = new ArrayList<>();
+        StringBuilder auxiliary = new StringBuilder();
         int numberOfStrings = words.length;
         int lengthOfEachString = words[0].length();
         int chunkLength = numberOfStrings * lengthOfEachString;
 
-        for (int i = 0; i < s.length(); i++) {
-            answer.append(s.charAt(i));
-            if (answer.length() % chunkLength == 0) {
-                // FUNCTION CALL
-            }
+        for (int i = 0; i < s.length(); i = i + lengthOfEachString) {
+            try { auxiliary.append(s, i, i + chunkLength); }
+            catch (IndexOutOfBoundsException e) { break; }
         }
+
+        return answer;
     }
 
     public static void main(String[] args) {
