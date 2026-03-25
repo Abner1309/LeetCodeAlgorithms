fcb77b11e34c4707c5023ac8b02b366adca50106
Abner1309 <soul_abner@hotmail.com>
2026-03-25 14:51:08 -0300

    Solution

--- 31) NextPermutation/Solution.java
+++ 31) NextPermutation/Solution.java
@@ -1,35 +1,48 @@
 import java.util.Arrays;
 
 public class Solution {
-    private int findIndex(int[] nums, int start, int value) {
-        int i = start;
+    private static int findIndexElement(int[] nums, int start, int value) {
+        int i = start, j = -1;
+        int target = Integer.MAX_VALUE;
 
         while (i < nums.length) {
-            if (nums[i] == value) { return i; }
+            if (nums[i] > value && nums[i] < target) {
+                target = nums[i];
+                j = i;
+            }
+            i++;
         }
 
-        return -1;
+        return j;
     }
 
-    private void swapElements (int[] nums, int index1, int index2) {
+    private static void swapElements (int[] nums, int index1, int index2) {
         int temp = nums[index1];
         nums[index1] = nums[index2];
         nums[index2] = temp;
     }
 
-    private void partialSort (int[] nums, int start) {
-        // PASS
+    private static void partialSort (int[] nums, int start) {
+        int i = start, j = i + 1;
+
+        while (j < nums.length) {
+            if (nums[i] > nums[j]) {
+                swapElements(nums, i, j);
+                i = start; j = i + 1;
+            }
+            else {
+                i++; j++;
+            }
+        }
     }
 
-    public void nextPermutation(int[] nums) {
+    public static void nextPermutation(int[] nums) {
         int firstPointer = nums.length - 2;
         int secondPointer = nums.length - 1;
 
-        int auxiliary = -1;
         while (firstPointer >= 0) {
             if (nums[firstPointer] < nums[secondPointer]) {
-                auxiliary = findIndex(nums, firstPointer, nums[firstPointer] + 1);
-                swapElements(nums, firstPointer, auxiliary);
+                swapElements(nums, firstPointer, findIndexElement(nums, firstPointer + 1, nums[firstPointer]));
                 partialSort(nums, firstPointer + 1);
                 return;
             }
@@ -38,5 +51,12 @@ public class Solution {
         }
 
         Arrays.sort(nums);
+    }
+
+    public static void main(String[] args) {
+        int[] vet = {2, 3, 1};
+        System.out.println(Arrays.toString(vet));
+        nextPermutation(vet);
+        System.out.println(Arrays.toString(vet));
     }
 }
--- README.md
+++ README.md
@@ -196,3 +196,18 @@ A concatenated string is a string that exactly contains all the strings of any p
     For example, if words = ["ab","cd","ef"], then "abcdef", "abefcd", "cdabef", "cdefab", "efabcd", and "efcdab" are all concatenated strings. "acdbef" is not a concatenated string because it is not the concatenation of any permutation of words.
 
 Return an array of the starting indices of all the concatenated substrings in s. You can return the answer in any order.
+___
+### (31) Next Permutation - Medium
+A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+
+    For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+
+The next permutation of an array of integers is the next lexicographically greater permutation of its integer. More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, then the next permutation of that array is the permutation that follows it in the sorted container. If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+
+    For example, the next permutation of arr = [1,2,3] is [1,3,2].
+    Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+    While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+
+Given an array of integers nums, find the next permutation of nums.
+
+The replacement must be in place and use only constant extra memory.
